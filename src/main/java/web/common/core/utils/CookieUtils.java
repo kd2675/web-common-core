47c623427e613ea05bed5674e9d194522824125b
김도영 <zz01064842675@gmail.com>
2026-02-02 15:44:06 +0900

    refactor: 쿠키 생성 및 삭제 로직 CookieUtils로 리팩토링

--- src/main/java/web/common/core/utils/CookieUtils.java
+++ src/main/java/web/common/core/utils/CookieUtils.java
@@ -13,36 +13,25 @@ public class CookieUtils {
     private static final String CONTEXT_DOMAIN = "";
 
     public static void createCookie(String name, String value) {
-        HttpServletResponse res = HttpUtils.getResponse();
-
-        try {
-            value = URLEncoder.encode(value, "UTF-8");
-
-            log.error("create cookie");
-            Cookie c = new Cookie(name, value);
-            c.setPath("/");
-            c.setMaxAge(60 * 60 * 24 * 1);
-//            c.setComment(name);
-            c.setHttpOnly(true);
-            c.setDomain(CONTEXT_DOMAIN);
-            res.addCookie(c);
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
+        createCookie(name, value, 60 * 60 * 24, false);
     }
 
     public static void createCookie(String name, String value, int maxAge) {
+        createCookie(name, value, maxAge, false);
+    }
+
+    public static void createCookie(String name, String value, int maxAge, boolean secure) {
         HttpServletResponse res = HttpUtils.getResponse();
 
         try {
             value = URLEncoder.encode(value, "UTF-8");
 
-            log.error("create cookie");
+            log.debug("create cookie: {}", name);
             Cookie c = new Cookie(name, value);
             c.setPath("/");
             c.setMaxAge(maxAge);
-//            c.setComment(name);
             c.setHttpOnly(true);
+            c.setSecure(secure);
             c.setDomain(CONTEXT_DOMAIN);
             res.addCookie(c);
         } catch (UnsupportedEncodingException e) {
@@ -53,14 +42,12 @@ public class CookieUtils {
     public static void deleteCookie(String name) {
         HttpServletResponse res = HttpUtils.getResponse();
 
-        log.error("delete cookie");
+        log.debug("delete cookie: {}", name);
         Cookie c = new Cookie(name, "");
         c.setPath("/");
         c.setMaxAge(0);
-//        c.setComment(name);
         c.setHttpOnly(true);
         c.setDomain(CONTEXT_DOMAIN);
         res.addCookie(c);
-        //testtesttest
     }
 }
